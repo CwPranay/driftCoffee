b25f8b091dd363db79f2d64d684e9ed54e079c8d
Pranay <guravpranay81@gmail.com>
2026-03-28 04:26:56 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://mnjjeso

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'mnjjesosykmgfmnfbwaz.supabase.co',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
